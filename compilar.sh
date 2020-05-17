3ec1ba38434356968e70f0b15919eee523dcbc36
Matheus Machado <matheusmachado708@gmail.com>
2020-05-17 12:30:55 -0300

    Método de Newton e complemento na 1.2

--- compilar.sh
+++ compilar.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#latex -output-directory=saida/ Relatorio.tex \
+#;
 pdflatex -output-directory=saida/ -output-format=pdf Relatorio.tex \
 && \
 pdflatex -output-directory=saida/ -output-format=pdf Relatorio.tex
